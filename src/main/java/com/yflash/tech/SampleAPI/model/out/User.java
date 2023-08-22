c7034269536309babb26fb3f8756437a30b1a3a7
samyak <samyakagarwal20@gmail.com>
2023-08-22 13:54:21 +0530

    added changes for base structure of the application

--- src/main/java/com/yflash/tech/SampleAPI/model/out/User.java
+++ src/main/java/com/yflash/tech/SampleAPI/model/out/User.java
@@ -2,10 +2,11 @@ package com.yflash.tech.SampleAPI.model.out;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
 
 @Getter
 @Setter
-public class User {
+public class User extends RepresentationModel<User> {
 
     Integer id;
     String firstName;
